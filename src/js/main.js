6c8c3418d8e078a315149e225f21fb8d10e9eca1
MaksimR02 <resetnak32@gmail.com>
2026-04-29 14:20:23 +0400

    feat: add sidebar and content navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
-console.log("Dashboard app started");
+const sidePanel = document.querySelector(".side-panel");
+const toggleButton = document.querySelector("#toggle-button");
+const openButton = document.querySelector("#open-button");
+
+const projectContent = document.querySelector("#project-content");
+const employeesContent = document.querySelector("#employees-content");
+const employeesButton = document.querySelector("#nav-project-employees");
+const projectButton = document.querySelector("#nav-project");
+
+toggleButton.addEventListener("click", () => {
+  sidePanel.classList.add("collapsed");
+  openButton.classList.remove("hidden");
+});
+
+openButton.addEventListener("click", () => {
+  openButton.classList.add("hidden");
+  sidePanel.classList.remove("collapsed");
+});
+
+projectButton.addEventListener("click", () => {
+  event.preventDefault();
+
+  employeesContent.classList.add("hidden");
+  projectContent.classList.remove("hidden");
+  employeesButton.classList.remove("active");
+  projectButton.classList.add("active");
+});
+
+employeesButton.addEventListener("click", () => {
+  event.preventDefault();
+
+  projectContent.classList.add("hidden");
+  employeesContent.classList.remove("hidden");
+  projectButton.classList.remove("active");
+  employeesButton.classList.add("active");
+});
